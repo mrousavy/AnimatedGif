c3e1360313fcb32e3b3baa40884e9a79a204a63c
Marc Rousavy <Marc Rousavy>
2017-04-27 11:41:38 +0200

    Project files added

--- AnimatedGif/Class1.cs
+++ AnimatedGif/Class1.cs
@@ -4,9 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AnimatedGif
-{
-    public class Class1
-    {
+namespace AnimatedGif {
+    public class Class1 {
     }
 }
