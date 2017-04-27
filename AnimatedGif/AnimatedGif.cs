12b931235d0bab9bea8f7aed8c56ae44b0fcdebd
Marc Rousavy <Marc Rousavy>
2017-04-27 12:04:39 +0200

    Improvements

---
+++ AnimatedGif/AnimatedGif.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Media.Imaging;
+
+namespace AnimatedGif {
+    public static class AnimatedGif {
+
+        /// <summary>
+        /// Create a new Animated GIF
+        /// </summary>
+        /// <param name="filePath">The Path where the Animated GIF gets saved</param>
+        /// <param name="delay">Delay between frames</param>
+        /// <param name="repeat">GIF Repeat count (0 meaning forever)</param>
+        /// <returns></returns>
+        public static AnimatedGifCreator Create(string filePath, int delay, int repeat = 0) => new AnimatedGifCreator(filePath, delay, repeat);
+
+        /// <summary>
+        /// Load an Animated GIF from File (not yet custom implemented)
+        /// </summary>
+        /// <param name="filePath">Path to GIF File</param>
+        /// <returns>GifBitmapDecoder using the GIF File</returns>
+        public static GifBitmapDecoder Load(string filePath) => new GifBitmapDecoder(new Uri(filePath), BitmapCreateOptions.DelayCreation,
+                BitmapCacheOption.Default);
+    }
+}
--- AnimatedGif/Properties/AssemblyInfo.cs
+++ AnimatedGif/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AnimatedGif")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A high performance .NET library for reading and creating (animated) GIFs")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mrousavy")]
 [assembly: AssemblyProduct("AnimatedGif")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("mrousavy")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
